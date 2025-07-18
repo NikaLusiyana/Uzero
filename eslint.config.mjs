54958fded1b332d4d01a667db4cfedca2966d204
NikaLusiyana <nikalusiyana112@gmail.com>
2025-07-18 14:21:51 +0700

    chore(eslint): migrate to Flat Config format and ignore generated files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginNext from '@next/eslint-plugin-next'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  // Konfigurasi dari Next.js
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
 
-export default eslintConfig;
+  // Plugin manual dari CommonJS
+  {
+    plugins: {
+      next: pluginNext,
+    },
+    rules: {
+      ...pluginNext.configs.recommended.rules, // kamu bisa pakai ini sebagai dasar
+    },
+  },
+
+  // Ignored folders
+  {
+    ignores: ['node_modules', '.next', 'dist', 'src/generated'],
+  },
+]
